4a061cfefcbb5ae595e03a03262f00ae4060eda9
banakemi <banakemi@gmail.com>
2022-01-17 15:18:45 +0900

    novatiq用のテストログ削除

--- modules/adgenerationBidAdapter.js
+++ modules/adgenerationBidAdapter.js
@@ -50,9 +50,6 @@ export const spec = {
         data = tryAppendQueryString(data, 'imark', '1');
       }
       data = tryAppendQueryString(data, 'tp', bidderRequest.refererInfo.referer);
-      console.log(`window.nlocvalue = ${window.nlocvalue}`);
-      console.log(`validReq.userId.novatiq =↓↓↓↓↓↓↓`);
-      console.log(validReq.userId.novatiq);
       const hyperId = window.nlocvalue === 1 && validReq.userId.novatiq ? validReq.userId.novatiq.snowflake : null;
       if (hyperId != null) {
         data = tryAppendQueryString(data, 'hyper_id', hyperId);
